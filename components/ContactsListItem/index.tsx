37871fbb303b04a85b8cee4ff98539d4bfed38e4
Vasini Lele <raffaelevasini@gmail.com>
2021-07-24 10:36:15 +0200

    check if already exist chatroom in contactList

--- components/ContactsListItem/index.tsx
+++ components/ContactsListItem/index.tsx
@@ -7,6 +7,7 @@ import { useNavigation } from "@react-navigation/native";
 import { API, graphqlOperation } from "@aws-amplify/api";
 import { Auth } from '@aws-amplify/auth';
 import { createChatRoom, createChatRoomUser } from "../../src/graphql/mutations";
+import { getUser } from "./query";
 
 export type ContactListItemProps = {
     user: User;
@@ -19,52 +20,97 @@ const ContactListItem = (props: ContactListItemProps) => {
 
     const onClick = async () => {
         try {
+
+            let checkUserChat = false;
+            let chatRoomID = "";
+            let chatName = "";
             //create or navigate to the chat room if exist
-            // 1 create new chat room
-            const newChatRoomData = await API.graphql(
-                graphqlOperation(
-                    createChatRoom, {
-                    input: {}
-                }
-                )
-            )
-            if (!newChatRoomData.data) {
-                console.log("createChatRoom fail");
-                return;
-            }
-
-            const newChatRoom = newChatRoomData.data.createChatRoom;
-            //2 add user to chat room
-            const newUserChatRoom = await API.graphql(
-                graphqlOperation(
-                    createChatRoomUser, {
-                    input: {
-                        userID: user.id,
-                        chatRoomID: newChatRoom.id
-                    }
-
-
-                }
-                )
-            )
+            // TODO 0 check if chat room exist
+            //get all chat room of logged user
             const userInfo = await Auth.currentAuthenticatedUser();
-            // 3. add authenticated user to the chat room
-            const addAuthUserToChatRoom = await API.graphql(
+            const userChatRoom = await API.graphql(
                 graphqlOperation(
-                    createChatRoomUser, {
-                    input: {
-                        userID: userInfo.attributes.sub,
-                        chatRoomID: newChatRoom.id
-                    }
-
-
+                    getUser, {
+                    id: userInfo.attributes.sub,
+                    idUser: user.id
                 }
+
                 )
             )
-            navigation.navigate('ChatRoom',{
-                id: newChatRoom.id,
-                name:"Hardcoded name"
-            })
+            let searchChat = userChatRoom.data.getUser.chatRoomUser.items;
+            //search if user is in chat room 
+            //TODO
+            //N.B. there is a better way to do this (not foreach)
+            //but with Graphql need to learn more
+            searchChat.forEach((element: any) => {
+
+                let userInChat = element.chatRoom.chatRoomUsers.items;
+                userInChat.forEach((element: any) => {
+
+                    if (element.user.id === user.id) {
+                        checkUserChat = true;
+                        chatName = element.user.name;
+                        chatRoomID = element.chatRoomID;
+                    }
+                });
+            });
+
+            if (checkUserChat) {
+                console.log("chatroom already exist");
+                //redirect to chat room exist
+                navigation.navigate('ChatRoom', {
+                    id: chatRoomID,
+                    name: chatName
+                })
+            } else {
+                //if not exist create new chat room
+                // 1 create new chat room
+                const newChatRoomData = await API.graphql(
+                    graphqlOperation(
+                        createChatRoom, {
+                        input: {}
+                    }
+                    )
+                )
+                if (!newChatRoomData.data) {
+                    console.log("createChatRoom fail");
+                    return;
+                }
+
+                const newChatRoom = newChatRoomData.data.createChatRoom;
+                //2 add user to chat room
+                const newUserChatRoom = await API.graphql(
+                    graphqlOperation(
+                        createChatRoomUser, {
+                        input: {
+                            userID: user.id,
+                            chatRoomID: newChatRoom.id
+                        }
+
+
+                    }
+                    )
+                )
+
+                // 3. add authenticated user to the chat room
+                const addAuthUserToChatRoom = await API.graphql(
+                    graphqlOperation(
+                        createChatRoomUser, {
+                        input: {
+                            userID: userInfo.attributes.sub,
+                            chatRoomID: newChatRoom.id
+                        }
+
+
+                    }
+                    )
+                )
+                navigation.navigate('ChatRoom', {
+                    id: newChatRoom.id,
+                    name: "Hardcoded name"
+                })
+
+            }
         } catch (error) {
             console.log(error);
         }
---
+++ components/ContactsListItem/query.ts
@@ -0,0 +1,35 @@
+//this qeury check if userLogged have already a chatRoom whit user
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      name
+      imageUri
+      status
+      chatRoomUser {
+        items {
+          id
+          userID
+          chatRoomID
+          createdAt
+          updatedAt
+          chatRoom{
+           id
+           chatRoomUsers{
+             items{
+                chatRoomID
+               user{
+                 id
+                 name
+               }
+             }
+           }
+         }
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
