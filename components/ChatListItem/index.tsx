89f2a29349d98bcb42f5939873e9c3401fcef297
Vasini Lele <raffaelevasini@gmail.com>
2021-07-26 14:54:56 +0200

    ChatList API

--- components/ChatListItem/index.tsx
+++ components/ChatListItem/index.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, Image, TouchableWithoutFeedback } from "react-native";
 import ChatRooms from "../../constants/data/ChatRooms";
 import { ChatRoom } from "../../types";
 import styles from "./styles";
 import moment from "moment";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { Auth } from "aws-amplify";
 
 export type ChatListItemProps = {
     chatRoom: ChatRoom;
@@ -12,40 +14,56 @@ export type ChatListItemProps = {
 
 const ChatListItem = (props: ChatListItemProps) => {
     const { chatRoom } = props;
-
+    const [otherUser, setOtherUser] = useState(null);
     const navigation = useNavigation();
 
-    const user = chatRoom.users[1];
+    useEffect(() => {
+        const getOtherUser = async () => {
+            const userInfo = await Auth.currentAuthenticatedUser();
 
-  
-    const dateCreate = new Date(Date.parse(chatRoom.lastMessage.createdAt));
+            if (chatRoom.chatRoomUsers.items[0].user.id === userInfo.attributes.sub) {
+                setOtherUser(chatRoom.chatRoomUsers.items[1].user);
+            } else {
+                setOtherUser(chatRoom.chatRoomUsers.items[0].user);
+
+            }
+
+        }
+        getOtherUser();
+    }, [])
+
+    // const dateCreate = new Date(Date.parse(chatRoom.lastMessage.createdAt));
     const onClick = () => {
-       navigation.navigate('ChatRoom', {
-           id: chatRoom.id, 
-           name:user.name,
-           img: user.imageUri
+        navigation.navigate('ChatRoom', {
+            id: chatRoom.id,
+            name: otherUser.name,
+            img: otherUser.imageUri
         });
     }
 
+    if (!otherUser) {
+        return null;
+    }
     return (
         <TouchableWithoutFeedback onPress={onClick}>
-              <View style={styles.container}>
-            <View style={styles.leftContainer}>
-                <Image source={{ uri: user.imageUri }} style={styles.avatar} />
-               
-                <View style={styles.midContainer}>
-                    <Text style={styles.username}>{user.name}</Text>
-                    <Text numberOfLines={2} style={styles.lastMessage}>{chatRoom.lastMessage.content}</Text>
-                </View>
+            <View style={styles.container}>
+                <View style={styles.leftContainer}>
+                    <Image source={{ uri: otherUser.imageUri }} style={styles.avatar} />
 
-            </View>
-            <Text style={styles.time}>
-                {moment(chatRoom.lastMessage.createdAt).format("DD/MM/YYYY")}
+                    <View style={styles.midContainer}>
+                        <Text style={styles.username}>{otherUser.name}</Text>
+                        <Text numberOfLines={2} style={styles.lastMessage}>{chatRoom.lastMessage ? chatRoom.lastMessage.content : ""}</Text>
+                    </View>
+
+                </View>
+                <Text style={styles.time}>
+
+                    {chatRoom.lastMessage && moment(chatRoom.lastMessage.createdAt).format("DD/MM/YYYY")}
                 </Text>
-           {/* <Text style={styles.time}>{dateCreate.toLocaleDateString()}</Text>*/}
-        </View>
+                {/* <Text style={styles.time}>{dateCreate.toLocaleDateString()}</Text>*/}
+            </View>
         </TouchableWithoutFeedback>
-  
+
     )
 
 
--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -3,17 +3,46 @@ import {FlatList, StyleSheet } from 'react-native';
 import { View } from '../components/Themed';
 import ChatListItem from '../components/ChatListItem'; 
 
-//static data for exampe chat
-import ChatRooms from '../constants/data/ChatRooms';
 import NewMessageButton from '../components/NewMessageButton';
+import { useEffect } from 'react';
+import { API, graphqlOperation } from "@aws-amplify/api";
+import { Auth } from '@aws-amplify/auth';
+import { getUser } from './queries/queries';
+import { useState } from 'react';
+
+//static data for exampe chat
+//import ChatRooms from '../constants/data/ChatRooms';
 
 export default function ChatsScreen() {
+  
+  const [chatRooms, setChatRooms] = useState([]);
+
+  useEffect(() => {
+    const fetchChatRooms = async () => {
+      try {
+        const userInfo = await Auth.currentAuthenticatedUser();
+        const userData = await API.graphql(
+          graphqlOperation(
+            getUser, {
+              id: userInfo.attributes.sub,
+            }
+          )
+        )
+        console.log("HEHEHEHEHEHEEHEHEHEHEHEHEHEHEHEH");
+        setChatRooms(userData.data.getUser.chatRoomUser.items);
+        console.log(userData.data.getUser.chatRoomUser.items);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchChatRooms();
+  }, [])
   return (
     <View style={styles.container}>
       <FlatList 
       style={{width:'100%'}}
-      data={ChatRooms} 
-      renderItem={({item}) => <ChatListItem chatRoom={item} />}
+      data={chatRooms} 
+      renderItem={({item}) => <ChatListItem chatRoom={item.chatRoom} />}
       keyExtractor={(item) => item.id}
       />
       <NewMessageButton />
